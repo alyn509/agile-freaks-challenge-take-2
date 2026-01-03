ab0e4785bfd28bab3df6fbaac56793ff56e54ed0
Alin Cruceat <alyn_509@yahoo.com>
2026-01-03 21:49:37 +0200

    security cleanup

--- helpers/live_location.py
+++ helpers/live_location.py
@@ -1,9 +1,9 @@
 import requests
 import json
-
-from helpers.constants import IP_STACK_ACCESS_KEY
+import os
 
 def get_live_location():
+    IP_STACK_ACCESS_KEY = os.environ["IP_STACK_ACCESS_KEY"] 
     send_url = "http://api.ipstack.com/check?access_key={}".format(IP_STACK_ACCESS_KEY)
     geo_req = requests.get(send_url)
     geo_json = json.loads(geo_req.text)
