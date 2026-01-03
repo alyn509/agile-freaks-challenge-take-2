5edac64f3bff9fd3038f2f3d5e7e06f5e9e05ee3
Alin Cruceat <alyn_509@yahoo.com>
2026-01-03 21:53:17 +0200

    add live test - test secret key well configured in github

---
+++ tests/test_closest_shops_live.py
@@ -0,0 +1,20 @@
+import unittest
+import pandas as pd
+from helpers.coffee_shop_finder import compute_closest_shops
+from helpers.live_location import get_live_location
+
+class TestClosestShops(unittest.TestCase):
+    def test_closest_shops_live(self):
+        data = pd.DataFrame({
+            'Name': ['Shop1', 'Shop2', 'Shop3', 'Shop4'],
+            'Y': [40.7128, 34.0522, 41.8781, 29.7604],
+            'X': [-74.0060, -118.2437, -87.6298, -95.3698]
+        })
+        y, x = get_live_location()
+        result = compute_closest_shops(data, y, x, n=2)
+        self.assertEqual(len(result), 2)
+        self.assertIn('distance', result.columns)
+        self.assertLessEqual(result.iloc[0]['distance'], result.iloc[1]['distance'])
+
+if __name__ == "__main__":
+    unittest.main()
